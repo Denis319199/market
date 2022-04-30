3fb12762caab33a5794e7658c81aecd102ea6a7c
= <=>
2022-04-30 22:57:17 +0300

    Specified classes for SpringBootTest in order to pass tests on build

--- auth/src/test/java/com/db/authservice/AuthServiceApplicationTests.java
+++ auth/src/test/java/com/db/authservice/AuthServiceApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = AuthorizationService.class)
 class AuthServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
--- market/src/test/java/com/db/marketplace/MarketApplicationTests.java
+++ market/src/test/java/com/db/marketplace/MarketApplicationTests.java
@@ -1,13 +1,12 @@
 package com.db.marketplace;
 
+import com.db.app.MarketApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MarketApplication.class)
 class MarketApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
